0269ea223fb98d9ee9a0831fc06d76301d1fd1f0
Glauco Veiga <veiga.cg@gmail.com>
2022-11-15 20:42:14 -0300

    Concluido

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,15 +1,42 @@
 const Tought = require('../models/Tought');
 const User = require('../models/User');
 
+const { Op } = require('sequelize');
+
 module.exports = class ToughtsController {
   static async showToughts(req, res) {
+    let search = '';
+
+    if (req.query.search) {
+      search = req.query.search;
+    }
+
+    let order = 'DESC';
+
+    if (req.query.order === 'old') {
+      order = 'ASC';
+    } else {
+      order = 'DESC';
+    }
+
     const toughtsData = await Tought.findAll({
       include: User,
+      where: {
+        // metodo de Search
+        title: { [Op.like]: `%${search}%` },
+      },
+      order: [['createdAt', order]],
     });
 
     const toughts = toughtsData.map((result) => result.get({ plain: true }));
 
-    res.render('toughts/home', { toughts });
+    let toughtsQty = toughts.length;
+
+    if (toughtsQty === 0) {
+      toughtsQty = false;
+    }
+
+    res.render('toughts/home', { toughts, search, toughtsQty });
   }
 
   // buscar conteudo do BD
