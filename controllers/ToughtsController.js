505478fdebf16613db95d37e5e35846461df9aa6
Glauco Veiga <veiga.cg@gmail.com>
2022-11-15 19:10:16 -0300

    Editi

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -26,7 +26,14 @@ module.exports = class ToughtsController {
     // pega o conteudo e filtar com map em uma Array
     const toughts = user.Toughts.map((result) => result.dataValues);
 
-    res.render('toughts/dashboard', { toughts });
+    let emptyToughts = false;
+
+    // verificar a quantidade de elementos no arr
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
+
+    res.render('toughts/dashboard', { toughts, emptyToughts });
   }
 
   static createTought(req, res) {
@@ -49,5 +56,31 @@ module.exports = class ToughtsController {
     } catch (error) {
       console.log('Aconteceu um erro:' + error);
     }
+  }
+
+  // Remover Conteudo
+  static async removeTought(req, res) {
+    const id = req.body.id;
+    const UserId = req.session.userid;
+
+    try {
+      await Tought.destroy({ where: { id: id, UserId: UserId } });
+
+      req.flash('message', 'Pensamento removido com sucesso!');
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch (error) {
+      console.log('Aconteceu um erro:' + error);
+    }
+  }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render('toughts/edit', { tought });
   }
 };
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -10,6 +10,12 @@ router.get('/dashboard', checkAuth, ToughtsController.dashboard);
 router.get('/add', checkAuth, ToughtsController.createTought);
 router.post('/add', checkAuth, ToughtsController.createToughtSave);
 
+// Edite Rotes
+router.get('/edit/:id', checkAuth, ToughtsController.updateTought);
+
+// Remove Rotes
+router.post('/remove', checkAuth, ToughtsController.removeTought);
+
 router.get('/', ToughtsController.showToughts);
 
 module.exports = router;
